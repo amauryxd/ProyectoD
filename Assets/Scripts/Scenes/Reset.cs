abb581f3953edd95bf038f79da103c5a4823ffc6
amauryxd <cdmx2472@amerike.edu.mx>
2023-05-24 12:23:40 -0600

    Reset arreglado

--- Assets/Scripts/Scenes/Reset.cs
+++ Assets/Scripts/Scenes/Reset.cs
@@ -7,7 +7,8 @@ public class Reset : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("Player")) { 
+        if (collision.CompareTag("Player")) {
+            PortalManager.PortalID = 0;
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
         }
     }
